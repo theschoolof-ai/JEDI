ccea3f2264af7016afebeaf5701af1041bd917fe
pranava sai <36731581+realpranav93@users.noreply.github.com>
2020-09-19 01:57:31 +0530

    Update Engine_train_test.py

--- tsai.jedi/Engine_train_test.py
+++ tsai.jedi/Engine_train_test.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 test_loss_ = []
 train_loss = 0
 
+criterion = nn.CrossEntropyLoss()
 
 def train(model, device, train_loader, optimizer, epoch, l1_regularization=[1, 0.001]):
     model.train()
@@ -24,9 +25,9 @@ def train(model, device, train_loader, optimizer, epoch, l1_regularization=[1, 0
             l1_regularization = 0
             for param in model.parameters():
                 l1_regularization += torch.sum(abs(param))
-            train_loss = F.nll_loss(output, target) + l * l1_regularization
+            train_loss = criterion(output, target) + l * l1_regularization
         else:
-            train_loss = F.nll_loss(output, target)
+            train_loss = criterion(output, target)
 
         train_loss.backward()
         optimizer.step()
@@ -47,7 +48,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
