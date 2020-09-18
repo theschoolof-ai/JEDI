84689aa881a9d668d4af1905bed5a6dc9a5b336d
pranav balasankula <pranavbalasankula@gmail.com>
2020-09-18 23:34:30 +0530

    making changes to config.py and dataloader.py to add trainloafer_cifar10 and input_size_cifar10

--- tsai.jedi/config.py
+++ tsai.jedi/config.py
@@ -1,9 +1,9 @@
 import torch
+
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 batch_size = 128
 num_workers = 1
 pin_memory = True
-input_size = (1,28,28)
-
-
+input_size_MNIST = (1, 28, 28)
+input_size_CIFAR10 = (1, 32, 32)
