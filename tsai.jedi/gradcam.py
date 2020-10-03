7d39e408e1d5580419191be9e4b53a1280ced771
thamizhannal <thamizhannal.p@gmail.com>
2020-10-03 08:58:17 -0700

    Added lr_finder.py,
    New file lr_finer.py added
    Engine_train_test.py - added train() returns train_acc, train_loss
    datatransforms.py - cutout added

--- tsai.jedi/gradcam.py
+++ tsai.jedi/gradcam.py
@@ -56,21 +56,6 @@ class ModelOutputs():
         return target_activations, x
 
 
-def preprocess_image(img):
-    means = [0.485, 0.456, 0.406]
-    stds = [0.229, 0.224, 0.225]
-
-    preprocessed_img = img.copy()[:, :, ::-1]
-    for i in range(3):
-        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - means[i]
-        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / stds[i]
-    preprocessed_img = \
-        np.ascontiguousarray(np.transpose(preprocessed_img, (2, 0, 1)))
-    preprocessed_img = torch.from_numpy(preprocessed_img)
-    preprocessed_img.unsqueeze_(0)
-    input = preprocessed_img.requires_grad_(True)
-    return input
-
 
 def show_cam_on_image(img, mask):
     heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
