1b968886f7685a078cfbba8ae2b16ad2758a6843
pranav balasankula <pranavbalasankula@gmail.com>
2020-10-17 00:22:35 +0530

    adding changes to code to read tinyimagenet data

--- tsai.jedi/dataloader.py
+++ tsai.jedi/dataloader.py
@@ -9,8 +9,11 @@ import matplotlib.pyplot as plt
 import pickle
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
-from datatransforms import train_transform_alb, test_transform_alb, train_transform_s11
+from datatransforms import train_transform_alb, test_transform_alb, train_transform_s11, train_transform_tinyimagenet, test_transform_tinyimagenet
 import config
+import time
+import cv2
+import PIL
 
 torch.manual_seed(1)
 kwargs = {'num_workers': config.num_workers, 'pin_memory': config.pin_memory} if config.use_cuda else {}
@@ -103,3 +106,53 @@ test_loader_CIFAR10_alb = torch.utils.data.DataLoader(testset,
 
 train_loader_CIFAR10_s11 = torch.utils.data.DataLoader(trainset_s11,
                                                    batch_size=config.batch_size, shuffle=True, **kwargs)
+
+###TinyImagenet
+def get_id_dictionary():
+    id_dict = {}
+    for i, line in enumerate(open(path + 'wnids.txt', 'r')):
+        id_dict[line.replace('\n', '')] = i
+    return id_dict
+
+
+def get_class_to_id_dict():
+    id_dict = get_id_dictionary()
+    all_classes = {}
+    result = {}
+    for i, line in enumerate(open(path + 'words.txt', 'r')):
+        n_id, word = line.replace('\n', '').split('\t')[:2]
+        all_classes[n_id] = word
+    for key, value in id_dict.items():
+        result[value] = (key, all_classes[key])
+    return result
+
+
+def get_data(id_dict):
+    print('starting loading data')
+    train_data, test_data = [], []
+    train_labels, test_labels = [], []
+    t = time.time()
+    for key, value in id_dict.items():
+        train_data += [cv2.imread(path + 'train/{}/images/{}_{}.JPEG'.format(key, key, str(i))) for i in range(500)]
+        train_labels_ = np.array([[0] * 200] * 500)
+        train_labels_[:, value] = 1
+        train_labels += train_labels_.tolist()
+
+    for line in open(path + 'val/val_annotations.txt'):
+        img_name, class_id = line.split('\t')[:2]
+        test_data.append(cv2.imread(path + 'val/images/{}'.format(img_name)))
+        test_labels_ = np.array([[0] * 200])
+        test_labels_[0, id_dict[class_id]] = 1
+        test_labels += test_labels_.tolist()
+
+    print('finished loading data, in {} seconds'.format(time.time() - t))
+    return np.array(train_data), np.array(train_labels), np.array(test_data), np.array(test_labels)
+
+train_data, train_labels, test_data, test_labels = get_data(get_id_dictionary())
+
+trainset_tiny = train_transform_tinyimagenet(image_list=train_data, label=train_labels)
+train_loader_tinyimgnet_s12 = torch.utils.data.DataLoader(trainset_tiny,
+                                                   batch_size=config.batch_size, shuffle=True, **kwargs)
+testset_tiny = test_transform_tinyimagenet(image_list=test_data, label=test_labels)
+test_loader_tinyimgnet_s12 = torch.utils.data.DataLoader(testset_tiny,
+                                                  batch_size=config.batch_size, shuffle=True, **kwargs)
--- tsai.jedi/datatransforms.py
+++ tsai.jedi/datatransforms.py
@@ -81,3 +81,52 @@ class train_transform_s11(Dataset):
         label = self.label[i]
         label = torch.tensor(label, dtype=torch.long)
         return image, label
+
+
+class train_transform_tinyimagenet(Dataset):
+    def __init__(self, image_list, label):
+        self.image_list = image_list
+        self.label = label
+        self.aug = A.Compose([
+            A.PadIfNeeded(min_height=72, min_width=72,
+                             always_apply=True, border_mode=0, value=[0, 0, 0]),
+            A.RandomCrop(64, 64, p=1),
+            #torch.FlipLR(),
+            A.HorizontalFlip(p=0.5),
+            A.Rotate((-8.0, 8.0), p=0.5),
+            A.Cutout(num_holes=8, max_h_size=8, max_w_size=8, fill_value=[0.4914, 0.4822, 0.4465], p=0.5),
+            A.Normalize(mean=(0.4802, 0.4481, 0.3975), std=(0.2302, 0.2265, 0.2262))
+        ])
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, i):
+        image = Image.fromarray(self.image_list[i]).convert('RGB')
+        image = self.aug(image=np.array(image))['image']
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        image = torch.tensor(image, dtype=torch.float)
+        label = self.label[i]
+        label = torch.tensor(label, dtype=torch.long)
+        return image, label
+
+
+class test_transform_tinyimagenet(Dataset):
+    def __init__(self, image_list, label):
+        self.image_list = image_list
+        self.label = label
+        self.aug = A.Compose([
+            A.Normalize(mean=(0.4802, 0.4481, 0.3975), std=(0.2302, 0.2265, 0.2262))
+        ])
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, i):
+        image = Image.fromarray(self.image_list[i]).convert('RGB')
+        image = self.aug(image=np.array(image))['image']
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        image = torch.tensor(image, dtype=torch.float)
+        label = self.label[i]
+        label = torch.tensor(label, dtype=torch.long)
+        return image, label
